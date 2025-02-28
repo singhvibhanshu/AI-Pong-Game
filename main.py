7b6c13fa2909530e077e971f268f605ae46cf414
Vibhanshu Singh <135852806+singhvibhanshu@users.noreply.github.com>
2025-02-28 12:11:15 +0530

    minor fixes

--- main.py
+++ main.py
@@ -1,32 +1,34 @@
 import pygame
 from pong import Game
 
-width, height = 700, 500
+class PongGame:
+    def __init__(self, window, width, height):
+        self.game = Game(window, width, height)
+        self.right_paddle = self.game.right_paddle
+        self.left_paddle = self.game.left_paddle
+        self.ball = self.game.ball
 
-window = pygame.display.set_mode((width, height))
+    def test_ai(self):
+        run = True
+        clock = pygame.time.Clock()
+        while run:
+            clock.tick(60)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+                    break
 
-game = Game(window, width, height)
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_w]:
+                game.move_paddle(left=True, up=True)
+            if keys[pygame.K_s]:
+                game.move_paddle(left=True, up=False)
 
-run = True
-clock = pygame.time.Clock()
-while run:
-    clock.tick(60)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-            break
+            game_info = game.loop()
+            print(game_info.left_score, game_info.right_score)
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        game.move_paddle(left=True, up=True)
-    if keys[pygame.K_s]:
-        game.move_paddle(left=True, up=False)
+            game.loop()
+            game.draw(False, True)
+            pygame.display.update()
 
-    game_info = game.loop()
-    print(game_info.left_score, game_info.right_score)
-
-    game.loop()
-    game.draw(False, True)
-    pygame.display.update()
-
-pygame.quit()
+        pygame.quit()
