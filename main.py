b6dd1ceb73c5e40b0c232010667c22c61facbcfd
Vibhanshu Singh <135852806+singhvibhanshu@users.noreply.github.com>
2025-02-28 11:55:45 +0530

    minor fixes

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
 import pygame
 from pong import Game
 
-WIDTH, HEIGHT = 700, 500
+width, height = 700, 500
 
-window = pygame.display.set_mode((WIDTH, HEIGHT))
+window = pygame.display.set_mode((width, height))
 
-game = Game(window, WIDTH, HEIGHT)
+game = Game(window, width, height)
 
 run = True
+clock = pygame.time.Clock()
 while run:
+    clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
             break
 
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        game.move_paddle(left=True, up=True)
+    if keys[pygame.K_s]:
+        game.move_paddle(left=True, up=False)
+
+    game_info = game.loop()
+    print(game_info.left_score, game_info.right_score)
+
     game.loop()
-    game.draw()
+    game.draw(False, True)
     pygame.display.update()
 
 pygame.quit()
