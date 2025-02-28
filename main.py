29147f952aedd1bafa233cf9e38ee2a416e321ab
Vibhanshu Singh <135852806+singhvibhanshu@users.noreply.github.com>
2025-02-28 19:00:08 +0530

    fixes

--- main.py
+++ main.py
@@ -35,3 +35,10 @@ class PongGame:
 
         pygame.quit()
 
+if __name__ == "__main__":
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, "config.txt")
+
+    config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, neat.DefaultStagnation, config_path)
+
+    run_neat(config)
