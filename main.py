43aac65d1b91759b0a00fd0fa636e4851f9f55cd
Vibhanshu Singh <135852806+singhvibhanshu@users.noreply.github.com>
2025-02-28 11:35:27 +0530

    gameplay implemented

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
 import pygame
 from pong import Game
+
+WIDTH, HEIGHT = 700, 500
+
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+
+game = Game(window, WIDTH, HEIGHT)
+
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+            break
+
+    game.loop()
+    game.draw()
+    pygame.display.update()
+
+pygame.quit()
